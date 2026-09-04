098f54085c7861d08b19b69c454a3a7a050419e9
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-09-03 20:50:02 -0400

    use host drivers

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,6 +10,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/satty.svg
 export DESKTOP=/usr/share/applications/satty.desktop
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/satty /usr/bin/zenity
